c626c75c9e124e229c2ff8222ba37e5fdaed1344
swopnil <swopnil420@gmail.com>
2020-10-19 00:12:02 +0600

    Added the header of the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.mn.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
