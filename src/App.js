e90617861809bdef127d71c6b4ffd5f1c8f358a4
swopnil <swopnil420@gmail.com>
2020-10-20 00:55:52 +0600

    Made the menu component using the REACT class and state.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/Menu/Menu';
 
 class App extends Component {
 	render() {
 		return (
-			<div className='App'>
+			<div>
 				<Navbar dark color='primary'>
 					<div className='container'>
 						<NavbarBrand href='/'>Logo</NavbarBrand>
 					</div>
 				</Navbar>
+                <Menu />
 			</div>
 		);
 	}
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,32 +1,97 @@
 import React, { Component } from 'react';
+import { Media } from 'reactstrap';
 
-class Menu extends Component
-{
-    /**
-     * For the Menu component I defined the constractor
-     * It gets parameter called 'props'
-     * For the constractor 'props' I need to supply
-     * the super class with props and it is required
-     * whenever you define a class component
-     * @param {*} props 
-     */
+class Menu extends Component {
+	/**
+	 * For the Menu component I defined the constractor
+	 * It gets parameter called 'props'
+	 * For the constractor 'props' I need to supply
+	 * the super class with props and it is required
+	 * whenever you define a class component
+	 *
+	 * Define state here
+	 * @param {*} props
+	 */
 
-    constructor(props) 
-    {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
 
-    /**
-     * A class component of REACT I should implement a method
-     * called render. It will render view to the corresponding component
-     */
+		this.state = {
+			dishes: [
+				{
+					id: 0,
+					name: 'Uthappizza',
+					image: 'assets/images/uthappizza.png',
+					category: 'mains',
+					label: 'Hot',
+					price: '4.99',
+					description:
+						'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+				},
+				{
+					id: 1,
+					name: 'Zucchipakoda',
+					image: 'assets/images/zucchipakoda.png',
+					category: 'appetizer',
+					label: '',
+					price: '1.99',
+					description:
+						'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+				},
+				{
+					id: 2,
+					name: 'Vadonut',
+					image: 'assets/images/vadonut.png',
+					category: 'appetizer',
+					label: 'New',
+					price: '1.99',
+					description:
+						'A quintessential ConFusion experience, is it a vada or is it a donut?',
+				},
+				{
+					id: 3,
+					name: 'ElaiCheese Cake',
+					image: 'assets/images/elaicheesecake.png',
+					category: 'dessert',
+					label: '',
+					price: '2.99',
+					description:
+						'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+				},
+			],
+		};
+	}
 
-    render()
-    {
-        return (
+	/**
+	 * A class component of REACT I should implement a method
+	 * called render. It will render view to the corresponding component
+	 */
 
-        );
-    }
+	render() {
+		const menu = this.state.dishes.map((dish) => {
+			return (
+				<div key={dish.id} className='col-12 mt-5'>
+					<Media tag='li'>
+						<Media left middle>
+							<Media object src={dish.image} alt={dish.name} />
+							<Media body className='ml-5'>
+								<Media heading>{dish.name}</Media>
+								<p>{dish.description}</p>
+							</Media>
+						</Media>
+					</Media>
+				</div>
+			);
+		});
+
+		return (
+			<div className='container'>
+				<div className='row'>
+					<Media list>{menu}</Media>
+				</div>
+			</div>
+		);
+	}
 }
 
 /**
