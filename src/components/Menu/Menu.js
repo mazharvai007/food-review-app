28cb59bc30540976a2bfe1d1014c7781892fcdc5
swopnil <swopnil420@gmail.com>
2020-10-19 23:51:21 +0600

    Finished a basic structure of a REACT component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Menu extends Component
+{
+    /**
+     * For the Menu component I defined the constractor
+     * It gets parameter called 'props'
+     * For the constractor 'props' I need to supply
+     * the super class with props and it is required
+     * whenever you define a class component
+     * @param {*} props 
+     */
+
+    constructor(props) 
+    {
+        super(props);
+    }
+
+    /**
+     * A class component of REACT I should implement a method
+     * called render. It will render view to the corresponding component
+     */
+
+    render()
+    {
+        return (
+
+        );
+    }
+}
+
+/**
+ * Do not forget to export the component
+ */
+export default Menu;
