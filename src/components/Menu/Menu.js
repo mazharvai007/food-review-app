5f09084da2b249cf50c303048a626530e7a2a962
swopnil <swopnil420@gmail.com>
2020-10-20 00:56:52 +0600

    Made the menu component using the REACT class and state.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -74,11 +74,11 @@ class Menu extends Component {
 					<Media tag='li'>
 						<Media left middle>
 							<Media object src={dish.image} alt={dish.name} />
-							<Media body className='ml-5'>
-								<Media heading>{dish.name}</Media>
-								<p>{dish.description}</p>
-							</Media>
 						</Media>
+                        <Media body className='ml-5'>
+                            <Media heading>{dish.name}</Media>
+                            <p>{dish.description}</p>
+                        </Media>
 					</Media>
 				</div>
 			);
