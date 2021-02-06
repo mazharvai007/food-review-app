8927461b2de17857847573866e9ff49f2acb963f
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-02-06 20:55:19 +0600

    Implementing react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 import Main from './components/Main/Main';
 
 class App extends Component {
 	render() {
 		return (
-			<div>
+			<BrowserRouter>
 				<Main />
-			</div>
+			</BrowserRouter>
 		);
 	}
 }
