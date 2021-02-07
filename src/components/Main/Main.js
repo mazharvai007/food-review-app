1d1d5dd180b23d6b0eea7a74a944dfbd307afd62
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-02-07 09:20:57 +0600

    Implementing a single page application

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from '../Home/Home';
 import DishDetail from '../DishDetail/DishDetail';
-import { DISHES } from '../../shared/dishes';
+import { DISHES } from '../../shared/dishes_back';
+import { COMMENTS } from '../../shared/comments';
+import { PROMOTIONS } from '../../shared/promotions';
+import { LEADERS } from '../../shared/leaders';
 import Header from '../Header/Header';
 import Menu from '../Menu/Menu';
+import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 
 class Main extends Component {
@@ -13,15 +17,30 @@ class Main extends Component {
 
 		this.state = {
 			dishes: DISHES,
+			comments: COMMENTS,
+			promotions: PROMOTIONS,
+			leaders: LEADERS,
 		};
 	}
 
 	render() {
 		const HomePage = () => {
-			return(
-				<Home />
+			return (
+				<Home
+					dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+					promotion={
+						this.state.promotions.filter(
+							(promo) => promo.featured
+						)[0]
+					}
+					leader={
+						this.state.leaders.filter(
+							(leader) => leader.featured
+						)[0]
+					}
+				/>
 			);
-		}
+		};
 		return (
 			<>
 				<Header />
@@ -32,6 +51,7 @@ class Main extends Component {
 						path='/menu'
 						component={() => <Menu dishes={this.state.dishes} />}
 					/>
+					<Route exact path='/contact' component={Contact} />
 					<Redirect to='/home' />
 				</Switch>
 				<Footer />
