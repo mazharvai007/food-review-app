a624c1fa00e4e1b952ef5441f88fdb0ce2d57406
Mazharul <mazharul@Mazharuls-MacBook-Pro.local>
2021-02-13 22:21:28 +0600

    applying redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Main from './components/Main/Main';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore;
 
 class App extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<Main />
-			</BrowserRouter>
+			<Provider store={store}>
+				<BrowserRouter>
+					<div className='App'>
+						<Main />
+					</div>
+				</BrowserRouter>
+			</Provider>
 		);
 	}
 }
